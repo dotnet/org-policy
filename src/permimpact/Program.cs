02c5f7f6f681f78ff77b7794da4d6a7638ab0004
Immo Landwerth <immol@microsoft.com>
2019-10-30 20:22:29 -0700

    Improve filtering in what-if tool

--- src/permimpact/Program.cs
+++ src/permimpact/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Csv;
 using Microsoft.DotnetOrg.GitHubCaching;
 using Microsoft.DotnetOrg.Ospo;
-
+using Microsoft.DotnetOrg.Policies;
 using Mono.Options;
 
 namespace Microsoft.DotnetOrg.PermissionImpact
@@ -17,14 +17,18 @@ namespace Microsoft.DotnetOrg.PermissionImpact
         private static async Task Main(string[] args)
         {
             string orgName = null;
-            string teamName = null;
+            var repoNames = new List<string>();
+            var teamNames = new List<string>();
+            var userNames = new List<string>();
             string outputFileName = null;
             string cacheLocation = null;
             var help = false;
 
             var options = new OptionSet()
                 .Add("org=", "The {name} of the GitHub organization", v => orgName = v)
-                .Add("team=", "The {name} of the team to analyze impact for", v => teamName = v)
+                .Add("r|repo=", "The {name} of the repo to analyze impact for", v => repoNames.Add(v))
+                .Add("t|team=", "The {name} of the team to analyze impact for", v => teamNames.Add(v))
+                .Add("u|user=", "The {name} of the user to analyze impact for", v => userNames.Add(v))
                 .Add("o|output=", "The {path} where the output .csv file should be written to.", v => outputFileName = v)
                 .Add("cache-location=", "The {path} where the .json cache is located.", v => cacheLocation = v)
                 .Add("h|?|help", null, v => help = true, true)
@@ -75,10 +79,16 @@ namespace Microsoft.DotnetOrg.PermissionImpact
                 return;
             }
 
-            await RunAsync(orgName, teamName, outputFileName, cacheLocation);
+
+            await RunAsync(orgName, repoNames, teamNames, userNames, outputFileName, cacheLocation);
         }
 
-        private static async Task RunAsync(string orgName, string teamName, string outputFileName, string cacheLocation)
+        private static async Task RunAsync(string orgName,
+                                           List<string> repoNames,
+                                           List<string> teamNames,
+                                           List<string> userNames,
+                                           string outputFileName,
+                                           string cacheLocation)
         {
             Console.WriteLine("Loading org data...");
             var cachedOrg = await CachedOrg.LoadFromCacheAsync(orgName, cacheLocation);
@@ -88,7 +98,9 @@ namespace Microsoft.DotnetOrg.PermissionImpact
                 return;
             }
 
-            var teamFilter = CreateTeamFilter(cachedOrg, teamName);
+            var repoFilter = CreateRepoFilter(cachedOrg, repoNames);
+            var teamFilter = CreateTeamFilter(cachedOrg, teamNames);
+            var userFilter = CreateUserFilter(cachedOrg, userNames);
 
             Console.WriteLine("Loading Microsoft links...");
             var ospoClient = await OspoClientFactory.CreateAsync();
@@ -116,14 +128,17 @@ namespace Microsoft.DotnetOrg.PermissionImpact
             }
 
             var isForExcel = outputFileName == null;
-            var csvDocument = new CsvDocument("team", "repo", "user", "user-name", "user-email", "change");
+            var csvDocument = new CsvDocument("team", "repo", "user", "user-name", "user-email", "is-microsoft", "change", "repo-admins");
 
             using (var writer = csvDocument.Append())
             {
                 foreach (var userAccess in cachedOrg.Collaborators)
                 {
-                    var user = userAccess.User;
                     var repo = userAccess.Repo;
+                    var user = userAccess.User;
+
+                    if (!repoFilter(repo) || !userFilter(user))
+                        continue;
 
                     foreach (var team in cachedOrg.Teams)
                     {
@@ -136,12 +151,21 @@ namespace Microsoft.DotnetOrg.PermissionImpact
                         if (whatIfRemoved.IsUnchanged)
                             continue;
 
+                        var isMicrosoft = microsoftLink.ContainsKey(user.Login) ? "Yes" : "No";
+                        var repoAdmins = repo.GetAdministrators()
+                                             .Select(u => (Email: emailByUser[u], Name: nameByUser[u]))
+                                             .Where(t => !string.IsNullOrEmpty(t.Email))
+                                             .Select(t => $"{t.Name}<{t.Email}>");
+                        var repoAdminList = string.Join("; ", repoAdmins);
+
                         writer.WriteHyperlink(team.Url, team.Name, isForExcel);
                         writer.WriteHyperlink(repo.Url, repo.Name, isForExcel);
                         writer.WriteHyperlink(user.Url, user.Login, isForExcel);
                         writer.Write(nameByUser[user]);
                         writer.Write(emailByUser[user]);
+                        writer.Write(isMicrosoft);
                         writer.Write(change);
+                        writer.Write(repoAdminList);
                         writer.WriteLine();
                     }
                 }
@@ -153,12 +177,33 @@ namespace Microsoft.DotnetOrg.PermissionImpact
                 csvDocument.ViewInExcel();
         }
 
-        private static Func<CachedTeam, bool> CreateTeamFilter(CachedOrg cachedOrg, string filterText)
+        private static Func<CachedRepo, bool> CreateRepoFilter(CachedOrg cachedOrg, List<string> names)
         {
-            if (string.IsNullOrEmpty(filterText))
+            if (!names.Any())
                 return _ => true;
 
-            var names = filterText.Split(';');
+            var repoByName = cachedOrg.Repos.ToDictionary(r => r.Name, StringComparer.OrdinalIgnoreCase);
+            var includedRepos = new HashSet<CachedRepo>();
+            foreach (var name in names)
+            {
+                if (!repoByName.TryGetValue(name, out var repo))
+                {
+                    Console.Error.WriteLine($"warning: Repo '{name}' doesn't exist");
+                }
+                else
+                {
+                    includedRepos.Add(repo);
+                }
+            }
+
+            return includedRepos.Contains;
+        }
+
+        private static Func<CachedTeam, bool> CreateTeamFilter(CachedOrg cachedOrg, List<string> names)
+        {
+            if (!names.Any())
+                return _ => true;
+
             var teamByName = cachedOrg.Teams.ToDictionary(t => t.Name, StringComparer.OrdinalIgnoreCase);
             var includedTeams = new HashSet<CachedTeam>();
             foreach (var name in names)
@@ -174,6 +219,28 @@ namespace Microsoft.DotnetOrg.PermissionImpact
             }
 
             return includedTeams.Contains;
+        }
+
+        private static Func<CachedUser, bool> CreateUserFilter(CachedOrg cachedOrg, List<string> logins)
+        {
+            if (!logins.Any())
+                return _ => true;
+
+            var userByLogin = cachedOrg.Users.ToDictionary(u => u.Login, StringComparer.OrdinalIgnoreCase);
+            var includedUsers = new HashSet<CachedUser>();
+            foreach (var login in logins)
+            {
+                if (!userByLogin.TryGetValue(login, out var user))
+                {
+                    Console.Error.WriteLine($"warning: User '{login}' doesn't exist");
+                }
+                else
+                {
+                    includedUsers.Add(user);
+                }
+            }
+
+            return includedUsers.Contains;
         }
     }
 }
