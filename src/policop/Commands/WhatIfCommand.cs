7c7ea53f67ecacb667718931f6168d60f2514e35
Immo Landwerth <immol@microsoft.com>
2019-11-04 19:36:32 -0800

    Improve what-if command

--- src/policop/Commands/WhatIfCommand.cs
+++ src/policop/Commands/WhatIfCommand.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.Csv;
 using Microsoft.DotnetOrg.GitHubCaching;
+using Microsoft.DotnetOrg.Ospo;
+using Microsoft.DotnetOrg.PolicyCop.Reporting;
 
 using Mono.Options;
 
@@ -13,11 +16,12 @@ namespace Microsoft.DotnetOrg.PolicyCop.Commands
     internal sealed class WhatIfCommand : ToolCommand
     {
         private string _orgName;
-        private readonly List<string> _repoNames = new List<string>();
-        private readonly List<string> _teamNames = new List<string>();
-        private readonly List<string> _userNames = new List<string>();
+        private string _command;
+        private string _newPermissions;
+        private List<string> _activeTerms;
         private string _outputFileName;
         private bool _viewInExcel;
+        private readonly ReportContext _reportContext = new ReportContext();
 
         public override string Name => "what-if";
 
@@ -26,18 +30,23 @@ namespace Microsoft.DotnetOrg.PolicyCop.Commands
         public override void AddOptions(OptionSet options)
         {
             options.AddOrg(v => _orgName = v)
-                   .Add("r|repo=", "The {name} of the repo to analyze impact for", v => _repoNames.Add(v))
-                   .Add("t|team=", "The {name} of the team to analyze impact for", v => _teamNames.Add(v))
-                   .Add("u|user=", "The {name} of the user to analyze impact for", v => _userNames.Add(v))
+                   .Add("team-user-remove", "Checks what happens if the selected users are removed from the selected teams", v => _command = "team-user-remove")
+                   .Add("team-repo-grant=", "Checks what happens if the selected users are removed from the selected teams", v => { _command = "team-repo-grant"; _newPermissions = v; })
+                   .Add("r", "Lists repos", v => { _activeTerms = _reportContext.RepoTerms; })
+                   .Add("t", "Lists teams", v => { _activeTerms = _reportContext.TeamTerms; })
+                   .Add("u", "Lists user", v => { _activeTerms = _reportContext.UserTerms; })
+                   .Add("excel", "Shows the results in Excel", v => _viewInExcel = true)
+                   .Add("c", "Column names to include", v => _activeTerms = _reportContext.IncludedColumns)
+                   .Add("f", "Extra filters", v => _activeTerms = _reportContext.ColumnFilters)
                    .Add("o|output=", "The {path} where the output .csv file should be written to.", v => _outputFileName = v)
-                   .Add("excel", "Shows the results in Excel", v => _viewInExcel = true);
+                   .Add("<>", v => _activeTerms?.Add(v));
         }
 
         public override async Task ExecuteAsync()
         {
-            if (_outputFileName == null && !ExcelExtensions.IsExcelInstalled())
+            if (string.IsNullOrEmpty(_orgName))
             {
-                Console.Error.WriteLine("error: you must specify an output path because you don't have Excel.");
+                Console.Error.WriteLine($"error: --org must be specified");
                 return;
             }
 
@@ -55,118 +64,83 @@ namespace Microsoft.DotnetOrg.PolicyCop.Commands
                 return;
             }
 
-            var repoFilter = CreateRepoFilter(org, _repoNames);
-            var teamFilter = CreateTeamFilter(org, _teamNames);
-            var userFilter = CreateUserFilter(org, _userNames);
+            var linkSet = await OspoLinkSet.LoadFromCacheAsync();
 
-            var emailByUser = new Dictionary<CachedUser, string>();
-            var nameByUser = new Dictionary<CachedUser, string>();
-
-            var csvDocument = new CsvDocument("team", "repo", "user", "change");
-
-            using (var writer = csvDocument.Append())
+            if (linkSet == null)
             {
-                foreach (var userAccess in org.Collaborators)
-                {
-                    var repo = userAccess.Repo;
-                    var user = userAccess.User;
-
-                    if (!repoFilter(repo) || !userFilter(user))
-                        continue;
-
-                    foreach (var team in org.Teams)
-                    {
-                        if (!teamFilter(team))
-                            continue;
-
-                        var whatIfRemoved = userAccess.WhatIfRemovedFromTeam(team);
-                        var change = whatIfRemoved.ToString();
-
-                        if (whatIfRemoved.IsUnchanged)
-                            continue;
-
-                        writer.WriteHyperlink(team.Url, team.Name, _viewInExcel);
-                        writer.WriteHyperlink(repo.Url, repo.Name, _viewInExcel);
-                        writer.WriteHyperlink(user.Url, user.Login, _viewInExcel);
-                        writer.Write(change);
-                        writer.WriteLine();
-                    }
-                }
+                Console.Error.WriteLine("error: links not cached yet. Run cache-refresh or cache-links first.");
+                return;
             }
 
-            if (_outputFileName != null)
-                csvDocument.Save(_outputFileName);
+            if (_command == null)
+            {
+                Console.Error.WriteLine($"error: must specify an operation (e.g. --team-user-remove)");
+                return;
+            }
+
+            if (_command == "team-user-remove")
+            {
+                WhatIf(org, linkSet, null);
+            }
+            else if (_command == "team-repo-grant")
+            {
+                CachedPermission newPermission;
+                if (_newPermissions == "pull")
+                {
+                    newPermission = CachedPermission.Pull;
+                }
+                else if (_newPermissions == "push")
+                {
+                    newPermission = CachedPermission.Push;
+                }
+                else if (_newPermissions == "admin")
+                {
+                    newPermission = CachedPermission.Admin;
+                }
+                else
+                {
+                    Console.Error.WriteLine($"error: permission can be 'pull', 'push' or 'admin' but not '{_newPermissions}'");
+                    return;
+                }
+
+                WhatIf(org, linkSet, newPermission);
+            }
+            else
+            {
+                Debug.Assert(false);
+            }
+        }
+
+        private void WhatIf(CachedOrg org, OspoLinkSet linkSet, CachedPermission? newPermission)
+        {
+            var repoFilter = _reportContext.CreateRepoTermFilter();
+            var teamFilter = _reportContext.CreateTeamTermFilter();
+            var userFilter = _reportContext.CreateUserTermFilter();
+            var columnFilters = _reportContext.CreateColumnFilters();
+
+            var rows = org.Collaborators.Where(ua => userFilter(new ReportRow(user: ua.User, linkSet: linkSet)))
+                          .SelectMany(c => org.Teams.Where(t => teamFilter(new ReportRow(team: t))), (ua, t) => (UserAccess: ua, Team: t))
+                          .Where(t => repoFilter(new ReportRow(repo: t.UserAccess.Repo)))
+                          .Select(t => new ReportRow(repo: t.UserAccess.Repo,
+                                                      userAccess: t.UserAccess,
+                                                      team: t.Team,
+                                                      user: t.UserAccess.User,
+                                                      linkSet: linkSet,
+                                                      whatIfPermission: t.UserAccess.WhatIf(t.Team, newPermission)))
+                          .Where(r => !r.WhatIfPermission.Value.IsUnchanged)
+                          .Where(columnFilters)
+                          .ToArray();
+
+            var columns = _reportContext.GetColumns("r:name", "t:name", "u:login", "ua:change");
+            var document = _reportContext.CreateReport(rows, columns);
+
+            if (!string.IsNullOrEmpty(_outputFileName))
+                document.Save(_outputFileName);
 
             if (_viewInExcel)
-                csvDocument.ViewInExcel();
+                document.ViewInExcel();
             else
-                csvDocument.PrintToConsole();
-        }
-
-        private static Func<CachedRepo, bool> CreateRepoFilter(CachedOrg cachedOrg, List<string> names)
-        {
-            if (!names.Any())
-                return _ => true;
-
-            var repoByName = cachedOrg.Repos.ToDictionary(r => r.Name, StringComparer.OrdinalIgnoreCase);
-            var includedRepos = new HashSet<CachedRepo>();
-            foreach (var name in names)
-            {
-                if (!repoByName.TryGetValue(name, out var repo))
-                {
-                    Console.Error.WriteLine($"warning: Repo '{name}' doesn't exist");
-                }
-                else
-                {
-                    includedRepos.Add(repo);
-                }
-            }
-
-            return includedRepos.Contains;
-        }
-
-        private static Func<CachedTeam, bool> CreateTeamFilter(CachedOrg cachedOrg, List<string> names)
-        {
-            if (!names.Any())
-                return _ => true;
-
-            var teamByName = cachedOrg.Teams.ToDictionary(t => t.Name, StringComparer.OrdinalIgnoreCase);
-            var includedTeams = new HashSet<CachedTeam>();
-            foreach (var name in names)
-            {
-                if (!teamByName.TryGetValue(name, out var team))
-                {
-                    Console.Error.WriteLine($"warning: Team '{name}' doesn't exist");
-                }
-                else
-                {
-                    includedTeams.Add(team);
-                }
-            }
-
-            return includedTeams.Contains;
-        }
-
-        private static Func<CachedUser, bool> CreateUserFilter(CachedOrg cachedOrg, List<string> logins)
-        {
-            if (!logins.Any())
-                return _ => true;
-
-            var userByLogin = cachedOrg.Users.ToDictionary(u => u.Login, StringComparer.OrdinalIgnoreCase);
-            var includedUsers = new HashSet<CachedUser>();
-            foreach (var login in logins)
-            {
-                if (!userByLogin.TryGetValue(login, out var user))
-                {
-                    Console.Error.WriteLine($"warning: User '{login}' doesn't exist");
-                }
-                else
-                {
-                    includedUsers.Add(user);
-                }
-            }
-
-            return includedUsers.Contains;
+                document.PrintToConsole();
         }
     }
 }
---
+++ src/policop/Reporting/ReportContext.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Microsoft.Csv;
+
+namespace Microsoft.DotnetOrg.PolicyCop.Reporting
+{
+    // TODO: - create functions to filter early on repo, team, and user.
+    //       - should combine term filters and column filters
+    //       - create a single function called CreateRowFilter() that creates the combined filter for the row
+    //       - fix WhatIfCommand
+    internal sealed class ReportContext
+    {
+        public List<string> RepoTerms { get; } = new List<string>();
+        public List<string> TeamTerms { get; } = new List<string>();
+        public List<string> UserTerms { get; } = new List<string>();
+        public List<string> IncludedColumns { get; } = new List<string>();
+        public List<string> ColumnFilters { get; } = new List<string>();
+
+        public Func<ReportRow, bool> CreateRepoTermFilter()
+        {
+            return CreateTermFilters(RepoTerms, "r:name");
+        }
+
+        public Func<ReportRow, bool> CreateTeamTermFilter()
+        {
+            return CreateTermFilters(TeamTerms, "t:name", "t:full-name");
+        }
+
+        public Func<ReportRow, bool> CreateUserTermFilter()
+        {
+            return CreateTermFilters(UserTerms, "u:login", "u:name", "u:email");
+        }
+
+        public IReadOnlyList<ReportColumn> GetColumns(params string[] defaultColumns)
+        {
+            Debug.Assert(defaultColumns != null && defaultColumns.Length > 0);
+
+            if (IncludedColumns.Count == 0)
+                IncludedColumns.AddRange(defaultColumns);
+
+            var result = new List<ReportColumn>();
+            var hadErrors = false;
+
+            foreach (var qualifiedName in IncludedColumns)
+            {
+                var column = ReportColumn.Get(qualifiedName);
+                if (column != null)
+                {
+                    result.Add(column);
+                }
+                else
+                {
+                    Console.Error.WriteLine($"error: column '{qualifiedName}' isn't valid");
+                    hadErrors = true;
+                }
+            }
+
+            if (hadErrors)
+                Environment.Exit(1);
+
+            return result;
+        }
+
+        private static Func<ReportRow, bool> CreateTermFilters(IReadOnlyCollection<string> expressions, params string[] qualifiedNames)
+        {
+            var termFilters = ParseTermFilters(expressions, qualifiedNames);
+            return CreateDisjunctionFilter(termFilters);
+        }
+
+        public Func<ReportRow, bool> CreateColumnFilters()
+        {
+            var termFilters = ParseColumnFilters(ColumnFilters);
+
+            var hasErrors = termFilters.Any(kv => kv.Key == null);
+            if (hasErrors)
+                Environment.Exit(1);
+
+            return CreateConjunctionFilter(termFilters);
+        }
+
+        private static IReadOnlyCollection<KeyValuePair<ReportColumn, string>> ParseTermFilters(IReadOnlyCollection<string> expressions, string[] qualifiedNames)
+        {
+            var columns = new List<ReportColumn>();
+
+            foreach (var qualifiedName in qualifiedNames)
+            {
+                var column = ReportColumn.Get(qualifiedName);
+                Debug.Assert(column != null, $"Column {qualifiedName} is invalid");
+                columns.Add(column);
+            }
+
+            var result = new List<KeyValuePair<ReportColumn, string>>();
+
+            foreach (var column in columns)
+            {
+                foreach (var expression in expressions)
+                {
+                    result.Add(new KeyValuePair<ReportColumn, string>(column, expression));
+                }
+            }
+
+            return result;
+        }
+
+        private static IReadOnlyCollection<KeyValuePair<ReportColumn, string>> ParseColumnFilters(IReadOnlyCollection<string> expressions)
+        {
+            return expressions.Select(ParseColumnFilter).ToArray();
+        }
+
+        private static KeyValuePair<ReportColumn, string> ParseColumnFilter(string expression)
+        {
+            var indexOfEquals = expression.IndexOf("=");
+
+            string qualifiedName;
+            string value;
+
+            if (indexOfEquals < 0)
+            {
+                qualifiedName = expression.Trim();
+                value = "Yes";
+            }
+            else
+            {
+                qualifiedName = expression.Substring(0, indexOfEquals).Trim();
+                value = expression.Substring(indexOfEquals + 1).Trim();
+            }
+
+            var column = ReportColumn.Get(qualifiedName);
+            if (column == null)
+                Console.Error.WriteLine($"error: column '{qualifiedName}' isn't valid");
+
+            return new KeyValuePair<ReportColumn, string>(column, value);
+        }
+
+        private static Func<ReportRow, bool> CreateConjunctionFilter(IReadOnlyCollection<KeyValuePair<ReportColumn, string>> columnFilters)
+        {
+            if (columnFilters.Count == 0)
+                return _ => true;
+
+            return row =>
+            {
+                foreach (var columnFilter in columnFilters)
+                {
+                    if (!Evaluate(row, columnFilter))
+                        return false;
+                }
+
+                return true;
+            };
+        }
+
+        private static Func<ReportRow, bool> CreateDisjunctionFilter(IReadOnlyCollection<KeyValuePair<ReportColumn, string>> columnFilters)
+        {
+            if (columnFilters.Count == 0)
+                return _ => true;
+
+            return row =>
+            {
+                foreach (var columnFilter in columnFilters)
+                {
+                    if (Evaluate(row, columnFilter))
+                        return true;
+                }
+
+                return false;
+            };
+        }
+
+        private static bool Evaluate(ReportRow row, KeyValuePair<ReportColumn, string> columnFilter)
+        {
+            var column = columnFilter.Key;
+            var term = columnFilter.Value;
+            var text = column.GetValue(row) ?? string.Empty;
+
+            var wildcardAtStart = term.StartsWith("*");
+            var wildcardAtEnd = term.EndsWith("*");
+            var actualTerm = term.Trim('*');
+
+            if (wildcardAtStart && wildcardAtEnd)
+                return text.IndexOf(actualTerm, StringComparison.OrdinalIgnoreCase) >= 0;
+
+            if (wildcardAtStart)
+                return text.EndsWith(actualTerm, StringComparison.OrdinalIgnoreCase);
+
+            if (wildcardAtEnd)
+                return text.StartsWith(actualTerm, StringComparison.OrdinalIgnoreCase);
+
+            return text.Equals(actualTerm, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public CsvDocument CreateReport(IReadOnlyCollection<ReportRow> rows, IReadOnlyList<ReportColumn> columns)
+        {
+            if (rows.Count == 0 || columns.Count == 0)
+                return new CsvDocument();
+
+            var headers = columns.Select(h => h.QualifiedName);
+            var document = new CsvDocument(headers);
+
+            using (var writer = document.Append())
+            {
+                foreach (var row in rows)
+                {
+                    foreach (var column in columns)
+                    {
+                        var value = column.GetValue(row);
+                        writer.Write(value ?? string.Empty);
+                    }
+
+                    writer.WriteLine();
+                }
+            }
+
+            return document;
+        }
+    }
+}
