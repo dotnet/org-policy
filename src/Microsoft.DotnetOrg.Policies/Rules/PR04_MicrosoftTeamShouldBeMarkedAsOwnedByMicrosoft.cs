86bc44247ca0d5ed413106f4961d392156870f03
Immo Landwerth <immol@microsoft.com>
2019-12-04 09:31:19 -0800

    Allow external teams to grant triage permissions

--- src/Microsoft.DotnetOrg.Policies/Rules/PR04_MicrosoftTeamShouldBeMarkedAsOwnedByMicrosoft.cs
+++ src/Microsoft.DotnetOrg.Policies/Rules/PR04_MicrosoftTeamShouldBeMarkedAsOwnedByMicrosoft.cs
@@ -15,21 +15,21 @@ namespace Microsoft.DotnetOrg.Policies.Rules
 
         public override IEnumerable<PolicyViolation> GetViolations(PolicyAnalysisContext context)
         {
-            var allowedPermission = CachedPermission.Read;
+            var maxNonMicrosoftPermission = CachedPermission.Triage;
             var microsoftTeam = context.Org.GetMicrosoftTeam();
 
             foreach (var team in context.Org.Teams)
             {
                 var isOwnedByMicrosoft = team.IsOwnedByMicrosoft();
-                var grantsMoreThanPullAccessToMicrosoftRepo = team.Repos.Any(r => r.Permission != allowedPermission && r.Repo.IsOwnedByMicrosoft()); ;
+                var exceedsMaxForExternals = team.Repos.Any(r => r.Repo.IsOwnedByMicrosoft() && r.Permission > maxNonMicrosoftPermission);
 
-                if (!isOwnedByMicrosoft && grantsMoreThanPullAccessToMicrosoftRepo)
+                if (!isOwnedByMicrosoft && exceedsMaxForExternals)
                 {
                     yield return new PolicyViolation(
                         Descriptor,
                         title: $"Team '{team.Name}' must be owned by Microsoft",
                         body: $@"
-                            Team {team.Markdown()} grants at least one Microsoft-owned repo more than {allowedPermission.Markdown()} permissions. The team must be owned by Microsoft.
+                            Team {team.Markdown()} grants at least one Microsoft-owned repo more than {maxNonMicrosoftPermission.Markdown()} permissions. The team must be owned by Microsoft.
 
                             To indicate that the team is owned by Microsoft, ensure that one of the parent teams is {microsoftTeam.Markdown()}.
                         ",
