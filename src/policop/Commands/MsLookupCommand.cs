cba9f244630bfdec135e3c9d000378cbc5bf772e
Immo Landwerth <immol@microsoft.com>
2019-12-09 13:48:41 -0800

    Add command to lookup MS users

---
+++ src/policop/Commands/MsLookupCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.DotnetOrg.Ospo;
+
+using Mono.Options;
+
+namespace Microsoft.DotnetOrg.PolicyCop.Commands
+{
+    internal sealed class MsLookupCommand : ToolCommand
+    {
+        private readonly List<string> _terms = new List<string>();
+
+        public override string Name => "ms-lookup";
+
+        public override string Description => "Looks up a Microsoft user by alias, email, name or GitHub handle";
+
+        public override void AddOptions(OptionSet options)
+        {
+            options.Add("<>", v => _terms.Add(v));
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            if (_terms.Count == 0)
+            {
+                Console.Error.WriteLine("error: needs argument");
+                return;
+            }
+
+            var client = await OspoClientFactory.CreateAsync();
+            var linkSet = await client.GetAllAsync();
+
+            foreach (var link in linkSet.Links)
+            {
+                var anyMatch = _terms.Any(t => IsMatch(link, t));
+                if (!anyMatch)
+                    continue;
+
+                Console.WriteLine($"@{link.GitHubInfo.Login}");
+                Console.WriteLine($"{link.MicrosoftInfo.PreferredName}");
+                Console.WriteLine($"{link.MicrosoftInfo.Alias}");
+                Console.WriteLine($"{link.MicrosoftInfo.EmailAddress}");
+            }
+        }
+
+        private static bool IsMatch(OspoLink link, string term)
+        {
+            var login = term.StartsWith("@")
+                            ? term.Substring(1)
+                            : term;
+
+            var aliasText = term.Contains("@")
+                                ? term.Substring(0, term.IndexOf('@'))
+                                : term;
+
+            return string.Equals(link.GitHubInfo.Id.ToString(), term, StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(link.GitHubInfo.Login, login, StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(link.MicrosoftInfo.Alias, aliasText, StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(link.MicrosoftInfo.EmailAddress, term, StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(link.MicrosoftInfo.PreferredName, term, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
