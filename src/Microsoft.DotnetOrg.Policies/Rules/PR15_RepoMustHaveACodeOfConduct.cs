791a92e35d2a25dbac167fdf9e29042992e2414a
Immo Landwerth <immol@microsoft.com>
2020-04-10 07:55:53 -0700

    Enable PR15 for mono org

--- src/Microsoft.DotnetOrg.Policies/Rules/PR15_RepoMustHaveACodeOfConduct.cs
+++ src/Microsoft.DotnetOrg.Policies/Rules/PR15_RepoMustHaveACodeOfConduct.cs
@@ -16,9 +16,10 @@ namespace Microsoft.DotnetOrg.Policies.Rules
         public override async Task GetViolationsAsync(PolicyAnalysisContext context)
         {
             // TODO: Enable for other orgs
-            //       mono, xamarin, xamarinhq
+            //       xamarin, xamarinhq
             if (!string.Equals(context.Org.Name, "aspnet", StringComparison.OrdinalIgnoreCase) &&
-                !string.Equals(context.Org.Name, "dotnet", StringComparison.OrdinalIgnoreCase))
+                !string.Equals(context.Org.Name, "dotnet", StringComparison.OrdinalIgnoreCase) &&
+                !string.Equals(context.Org.Name, "mono", StringComparison.OrdinalIgnoreCase))
                 return;
 
             // This rule is not scoped to anyone because both Microsoft and .NET Foundation
