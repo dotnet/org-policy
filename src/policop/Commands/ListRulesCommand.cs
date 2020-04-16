17acf4645996278d683d26fcc8993b0ddcdda0dc
Immo Landwerth <immol@microsoft.com>
2020-04-15 19:45:00 -0700

    Add command to list rules

---
+++ src/policop/Commands/ListRulesCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.Csv;
+using Microsoft.DotnetOrg.Policies;
+
+using Mono.Options;
+
+namespace Microsoft.DotnetOrg.PolicyCop.Commands
+{
+    internal sealed class ListRulesCommand : ToolCommand
+    {
+        public override string Name => "list-rules";
+
+        public override string Description => "Shows the list of policy rules";
+
+        public override void AddOptions(OptionSet options)
+        {
+        }
+
+        public override Task ExecuteAsync()
+        {
+            var document = new CsvDocument("id", "severity", "title");
+            using (var writer = document.Append())
+            {          
+                foreach (var rule in PolicyRunner.GetRules())
+                {
+                    writer.Write(rule.Descriptor.DiagnosticId);
+                    writer.Write(rule.Descriptor.Severity.ToString());
+                    writer.Write(rule.Descriptor.Title);
+                    writer.WriteLine();
+                }
+            }
+
+            document.PrintToConsole();
+            return Task.CompletedTask;
+        }
+    }
+
+}
