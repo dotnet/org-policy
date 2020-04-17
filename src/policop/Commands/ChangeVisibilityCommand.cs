896101137a232a25461c3520a51cc45bf7f3f991
Immo Landwerth <immol@microsoft.com>
2020-04-16 18:19:19 -0700

    Add command to change repo visibility

---
+++ src/policop/Commands/ChangeVisibilityCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.DotnetOrg.GitHubCaching;
+
+using Mono.Options;
+
+using Octokit;
+
+namespace Microsoft.DotnetOrg.PolicyCop.Commands
+{
+    internal sealed class ChangeVisibilityCommand : ToolCommand
+    {
+        private string _orgName;
+        private string _repoName;
+        private bool _makePrivate;
+        private bool _makePublic;
+
+        public override string Name => "change-visibility";
+
+        public override string Description => "Makes a repo public or private";
+
+        public override void AddOptions(OptionSet options)
+        {
+            options.AddOrg(v => _orgName = v)
+                   .Add("r=", "Specifies the {name} of the repo", v => _repoName = v)
+                   .Add("private", "Makes the repo private", v => _makePrivate = true)
+                   .Add("public", "Makes the repo public", v => _makePublic = true);
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            if (string.IsNullOrEmpty(_orgName))
+            {
+                Console.Error.WriteLine($"error: --org must be specified");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(_repoName))
+            {
+                Console.Error.WriteLine($"error: -r must be specified");
+                return;
+            }
+
+            if (!(_makePrivate ^ _makePublic))
+            {
+                Console.Error.WriteLine($"error: must specify either --private or --public");
+                return;
+            }
+
+            var client = await GitHubClientFactory.CreateAsync();
+
+            var update = new RepositoryUpdate(_repoName);
+            update.Private = _makePrivate;
+
+            try
+            {
+                await client.Repository.Edit(_orgName, _repoName, update);
+            }
+            catch (ApiException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
