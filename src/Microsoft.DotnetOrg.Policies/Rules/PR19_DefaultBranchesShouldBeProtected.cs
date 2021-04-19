35003ffc03d90e1a4ac7d9cf221dc8cb19e99f09
Immo Landwerth <immol@microsoft.com>
2021-04-19 14:59:58 -0700

    Add PR19 and PR20
    These rules check that the default branch (PR19) and all release branches (PR20)
    have branch protection rules.
    
    Sadly, the GitHub API doesn't include the setting to indicate whether force
    pushes are disabled, which is probably the minimum we want to prescribe. Right
    now, the tool just flags branches in repos that don't have any matching rules.

---
+++ src/Microsoft.DotnetOrg.Policies/Rules/PR19_DefaultBranchesShouldBeProtected.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+
+namespace Microsoft.DotnetOrg.Policies.Rules
+{
+    internal sealed class PR19_DefaultBranchesShouldBeProtected : PolicyRule
+    {
+        public override PolicyDescriptor Descriptor { get; } = new PolicyDescriptor(
+            "PR19",
+            "Default branches should have branch protection",
+            PolicySeverity.Warning
+        );
+
+        public override void GetViolations(PolicyAnalysisContext context)
+        {
+            foreach (var repo in context.Org.Repos)
+            {
+                if (repo.IsFork || repo.IsTemporaryForkForSecurityAdvisory())
+                    continue;
+
+                if (repo.IsArchivedOrSoftArchived())
+                    continue;
+
+                if (!repo.IsOwnedByMicrosoft())
+                    continue;
+
+                // Let's ignore uninitialized repos
+                if (repo.DefaultBranch is null)
+                    continue;
+
+                if (!repo.DefaultBranch.Rules.Any())
+                {
+                    context.ReportViolation(
+                        Descriptor,
+                        $"The default branch '{repo.DefaultBranch.Name}' in '{repo.Name}' has no branch protection",
+                        $@"
+                            The default branch {repo.DefaultBranch.Markdown()} in repo {repo.Markdown()} should have branch protection rules, such as preventing force pushes and requiring PRs.
+                        ",
+                        repo: repo,
+                        branch: repo.DefaultBranch
+                    );
+                }
+            }
+        }
+    }
+}
