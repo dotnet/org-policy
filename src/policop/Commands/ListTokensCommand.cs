1eb51c4fc42bee94b6738cd2213e35614c89bbd5
Immo Landwerth <immol@microsoft.com>
2021-09-20 13:36:44 -0700

    Add command to list the tokens

---
+++ src/policop/Commands/ListTokensCommand.cs
@@ -0,0 +1,41 @@
+﻿using Mono.Options;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotnetOrg.PolicyCop.Commands
+{
+    internal sealed class ListTokensCommand : ToolCommand
+    {
+        public override string Name => "list-tokens";
+
+        public override string Description => "Lists the stored access tokens.";
+
+        public override void AddOptions(OptionSet options)
+        {
+        }
+
+        public override Task ExecuteAsync()
+        {
+            var directory = GetTokenDirectory();
+            if (Directory.Exists(directory))
+            {
+                foreach (var fileName in Directory.GetFiles(directory))
+                    Console.WriteLine(fileName);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static string GetTokenDirectory()
+        {
+            var exePath = Environment.GetCommandLineArgs()[0];
+            var fileInfo = FileVersionInfo.GetVersionInfo(exePath)!;
+            var companyName = fileInfo.CompanyName ?? string.Empty;
+            var productName = fileInfo.ProductName ?? string.Empty;
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), companyName, productName);
+        }
+    }
+}
