25a4e016fc8a1af4183e8a122f754d15f1455dc6
Immo Landwerth <immol@microsoft.com>
2021-09-20 13:28:39 -0700

    Add command to see current user

---
+++ src/policop/Commands/WhoAmICommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.DotnetOrg.GitHubCaching;
+
+using Mono.Options;
+
+namespace Microsoft.DotnetOrg.PolicyCop.Commands
+{
+    internal sealed class WhoAmICommand : ToolCommand
+    {
+        public override string Name => "whoami";
+
+        public override string Description => "Displays the GitHub user account being used";
+
+        public override void AddOptions(OptionSet options)
+        {
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            var client = await GitHubClientFactory.CreateAsync();
+            var me = await client.User.Current();
+            Console.WriteLine(me.Login);
+        }
+    }
+}
